3150ba11d44ce134936ef6b4468c3293a941ba52
Sharooll <smezav1@unicartagena.edu.co>
2026-09-18 06:16:08 -0500

    feat(ui): actualizar Main para integrar el modulo de examenes

--- src/main/java/com/universidad/academico/infrastructure/ui/Main.java
+++ src/main/java/com/universidad/academico/infrastructure/ui/Main.java
@@ -1,11 +1,16 @@
 package main.java.com.universidad.academico.infrastructure.ui;
 
+import java.time.LocalDate;
+import main.java.com.universidad.academico.application.usecase.RegisterExamenUseCase;
 import main.java.com.universidad.academico.application.usecase.RegisterProfesorUseCase;
 import main.java.com.universidad.academico.application.usecase.RegisterSubjectUseCase;
+import main.java.com.universidad.academico.domain.model.Examen;
 import main.java.com.universidad.academico.domain.model.Profesor;
 import main.java.com.universidad.academico.domain.model.Subject;
+import main.java.com.universidad.academico.domain.repository.ExamenRepository;
 import main.java.com.universidad.academico.domain.repository.ProfesorRepository;
 import main.java.com.universidad.academico.domain.repository.SubjectRepository;
+import main.java.com.universidad.academico.infrastructure.persistence.InMemoryExamenRepository;
 import main.java.com.universidad.academico.infrastructure.persistence.InMemoryProfesorRepository;
 import main.java.com.universidad.academico.infrastructure.persistence.InMemorySubjectRepository;
 
@@ -36,7 +41,7 @@ public class Main {
             System.out.println("Ocurrió un error: " + e.getMessage());
         }
 
-        System.out.println("*******");
+        System.out.println("<<33");
 
         // == Modulo profesores == //
 
@@ -59,6 +64,33 @@ public class Main {
             System.out.println("Error en Profesores: " + e.getMessage());
         }
         
-        System.out.println("=== EJECUCIÓN FINALIZADA EXITOSAMENTE ===");
+        System.out.println("=== Ejecución finalizada ===");
+
+        System.out.println(" <<33 ");
+
+        // == Modulo examenes == 
+
+        ExamenRepository examenRepository = new InMemoryExamenRepository();
+        RegisterExamenUseCase registerExamenUseCase = new RegisterExamenUseCase(examenRepository);
+
+        try {
+            Examen nuevoExamen = new Examen(
+                "EX-001",
+                "Parcial 1: Arquitectura Hexagonal",
+                "SIS-101",
+                LocalDate.of(2026, 4, 15),
+                30.0
+            );
+
+            registerExamenUseCase.execute(nuevoExamen);
+            System.out.println("Total de exámenes programados: " + examenRepository.findAll().size());
+
+        } catch (Exception e) {
+            System.out.println("Error en Exámenes: " + e.getMessage());
+        }
+        
+        System.out.println("=== Ejecucion finalizada ===");
+
+        System.out.println(" <<33 ");
     }
 }
